73007a667f7e0b1074d2493ff00ba2cacf1eea06
NikolaPedro <92205953+NikolaPedro@users.noreply.github.com>
2022-07-11 00:02:37 +1000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+mod basics;
+
+use basics::Vector3;
+use std::{fs::File, io::Write};
+
+
 fn main() {
-    println!("Hello, world!");
+    let first = Vector3::new(1.0, 2.0, 3.0);
+    let second = Vector3::new(4.0, 5.0, 6.0);
+    println!("{:?}", first.cross(&second).normalized());
+}
+
+fn draw_image() -> std::io::Result<()> {
+    let width = 1024;
+    let height = 512;
+    
+    let mut file = File::create("img.ppm")?;
+    file.write_all(format!("P3\n{} {}\n255\n", width, height).as_bytes())?;
+
+    for y in (0..height).rev() {
+        for x in 0..width {
+            let r = (x as f64) / (width as f64);
+            let g = (y as f64) / (height as f64);
+            let b = 0.5;
+            
+            let r = (256.0 * r) as u8;
+            let g = (256.0 * g) as u8;
+            let b = (256.0 * b) as u8;
+            file.write_all(format!("{} {} {}\n", r, g, b).as_bytes())?;
+        }
+    }
+    Ok(())
 }
